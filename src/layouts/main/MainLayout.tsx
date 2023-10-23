d72d73b394232565b67ef72e7b1670c75a42ecb0
Le Duc Tuan <tuanle@Les-Mac-mini.local>
2023-10-23 11:53:29 +0700

    fix layout tailwindcss

--- src/layouts/main/MainLayout.tsx
+++ src/layouts/main/MainLayout.tsx
@@ -1,29 +1,25 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from './header'
-import { Box, Container } from '@mui/material'
-import Footer from './footer'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './header';
+import { Box, Container } from '@mui/material';
+import Footer from './footer';
 
 export default function MainLayout() {
   return (
-    <Box sx={{display: 'flex', flexDirection: 'column', height: 1}}>
-
+    <div className="flex flex-col h-1">
       <Header />
-      
-      <Box
-        component='main'
-        sx={{flexGrow: 1, pt: 12}}
-      >
-        <Container>
 
-          <Outlet />
-          
-        </Container>
+      <main className="flex-grow pt-12">
+        <div className=" sticky px-[1/3] min-h-20 py-4 top-0 z-[20] flex w-full items-center justify-between border-gray-500">
+          <div className="px-6 max-w-[1200px] w-full ml-auto mr-auto block box-border">
+            
+            <Outlet />
 
-      </Box>
+          </div>
+        </div>
+      </main>
 
       <Footer />
-    
-    </Box>
-  )
+    </div>
+  );
 }
