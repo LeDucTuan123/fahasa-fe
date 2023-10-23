5d934a22254af66188d85d147e18e69dfb004c81
Le Duc Tuan <tuanle@Les-Mac-mini.local>
2023-10-23 17:52:05 +0700

    fix footer

--- src/sections/e-commerce/view/DetailProductView.tsx
+++ src/sections/e-commerce/view/DetailProductView.tsx
@@ -1,21 +1,15 @@
-import { Button, Stack } from '@mui/material'
-import React from 'react'
 import { Helmet } from 'react-helmet-async';
-import { Link, useNavigate } from 'react-router-dom'
 import { DetailProduct } from '../detailProduct';
 
 export default function DetailProductView() {
 
-    const navigate = useNavigate();
-
-    const handleBack = () => {
-        navigate('/')
-    }
-
   return (
     <>
-            <DetailProduct />
+      <Helmet>
+        <title>Sản phẩm chi tiết</title>
+      </Helmet>
 
+      <DetailProduct />
     </>
-  )
+  );
 }
