3726c9068466756dd3eb3e6fb732bdc3a4dafca9
tranquoctri1933 <tranquoctri1933@gmail.com>
2023-10-26 21:19:38 +0700

    filter

--- src/sections/e-commerce/products/Filter.tsx
+++ src/sections/e-commerce/products/Filter.tsx
@@ -1,7 +1,253 @@
-import React from 'react'
+import React from 'react';
 
 export default function filter() {
   return (
-    <div>filter</div>
-  )
+    <div>
+      <div className="border-2">
+        <div className="font-medium m-2">
+          NHÓM SẢN PHẨM
+          <div className="text-slate-400 ml-2 mt-1">Tất Cả Nhóm Sản Phẩm</div>
+          <div className="font-medium ml-4 mt-1 text-amber-400">Sách tiếng Việt</div>
+          <ol className="ml-6 mt-1 text-slate-400">
+            <li>Thiếu Nhi</li>
+            <li>Giáo Khoa - Tham Khảo</li>
+            <li>Văn Học</li>
+            <li>Tâm Lý - Kỹ Năng Sống</li>
+            <li>Manga - Comic</li>
+            <li>Sách Học Ngoại Ngữ</li>
+            <li>Kinh Tế</li>
+            <li>Khoa Học Kỹ Thuật</li>
+          </ol>
+        </div>
+        <div className="font-medium m-2">
+          GIÁ
+          <ol className="ml-2 mt-1 text-slate-400">
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                0đ - 150,000đ
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                150,000đ - 300,000đ
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                300,000đ - 500,000đ
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                500,000đ - 700,000đ
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                700,000đ - Trở lên
+              </a>
+            </li>
+          </ol>
+        </div>
+        <div className="font-medium m-2">
+          GENRES
+          <ol className="ml-2 mt-1 text-slate-400">
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                Comedy
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                Shounen
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                Adventure
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                Drama
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                Action
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                School Lìe
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                Horror
+              </a>
+            </li>
+          </ol>
+        </div>
+        <div className="font-medium m-2">
+          ĐỘ TUỔI
+          <ol className="ml-2 mt-1 text-slate-400">
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                0 - 6
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                15 - 18
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                1 - 6
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                15+
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                17+
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                3+
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                18+
+              </a>
+            </li>
+          </ol>
+        </div>
+        <div className="font-medium m-2">
+          NGÔN NGỮ
+          <ol className="ml-2 mt-1 text-slate-400">
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                Tiếng Việt
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                Song Ngữ Anh - Việt
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                Tiếng Anh
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                Tiếng Nhật
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                Tiếng Trung
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                Tiếng Đức
+              </a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="bg-[url('https://cdn0.fahasa.com/skin/frontend/base/default/images/manapro_filtercheckboxes/ico_uncheck.png')] bg-no-repeat pl-5"
+              >
+                Tiếng Hàn
+              </a>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
 }
