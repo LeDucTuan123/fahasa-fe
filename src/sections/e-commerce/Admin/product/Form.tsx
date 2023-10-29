d28b44cd01bc946c01189951d1ba285f2c4fcc6e
Le Duc Tuan <tuanleduc999@gmail.com>
2023-10-29 20:40:02 +0700

    aaaaaa

---
+++ src/sections/e-commerce/Admin/product/Form.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Form() {
+  return (
+    <div className="w-full h-[300px] shadow-xl">
+      <form action="">
+        <label htmlFor="">Tên</label>
+      </form>
+    </div>
+  );
+}
