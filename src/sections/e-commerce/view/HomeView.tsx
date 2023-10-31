aabc449ad05c59d43ca3d231144d096f4ed1b463
phuongphamduy <92977825+phuongphamduy@users.noreply.github.com>
2023-10-31 12:47:10 +0700

    move dispatch to main layout

--- src/sections/e-commerce/view/HomeView.tsx
+++ src/sections/e-commerce/view/HomeView.tsx
@@ -1,17 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Banner, BestSellingBooks, Category, LatestBooks } from '../home';
 import { BookType } from 'src/types/book';
-import { useAppDispatch } from 'src/redux/store';
-import { getBook } from 'src/redux/slice/bookSlice';
 import { useSelector } from 'react-redux';
 
 export default function HomeView() {
-  const dispatch = useAppDispatch();
   const books: BookType[] = useSelector((state: any) => state.book.books);
-
-  useEffect(() => {
-    dispatch(getBook());
-  }, [dispatch]);
 
   return (
     <>
