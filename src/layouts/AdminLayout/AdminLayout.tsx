e523905bbfedd2e62525d816d94f461e7d8959dd
Le Duc Tuan <tuanleduc999@gmail.com>
2023-11-07 13:04:13 +0700

    update admin

--- src/layouts/AdminLayout/AdminLayout.tsx
+++ src/layouts/AdminLayout/AdminLayout.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './sidebar';
 import Header from './header';
 
 export default function AdminLayout() {
+  const [isShowSidebar, setIsShowSidebar] = useState(true);
+
+  const handleShowSidebar = () => setIsShowSidebar(!isShowSidebar);
+
   return (
     <div className="p-0 m-0 w-full h-screen">
       <div className="flex flex-row">
-        <div className="w-[250px] h-screen bg-slate-900 ">
-          <Sidebar />
+        <div className={`${isShowSidebar ? 'w-[250px]' : 'w-[80px]'} duration-300 transform h-screen bg-slate-900 `}>
+          <Sidebar
+            isShowSidebar={isShowSidebar}
+            onHandleShowSidebar={handleShowSidebar}
+          />
         </div>
 
         <div className="w-full flex flex-1 h-screen ">
--- src/sections/e-commerce/Admin/index.ts
+++ src/sections/e-commerce/Admin/index.ts
@@ -1,4 +1,5 @@
 export { default as Dashboard } from './Dashboard';
 export { default as User } from './User';
-export { default as Feedback } from './Feedback';
+export { default as Bill } from './Bill';
 export { default as Orther } from './Orther';
+export { default as Voucher } from './voucher';
