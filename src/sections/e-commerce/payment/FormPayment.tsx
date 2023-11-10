787cc377ef500cf1e3edfd2721392a9edb088d07
phuongphamduy <92977825+phuongphamduy@users.noreply.github.com>
2023-11-10 23:41:08 +0700

    Update FormPayment.tsx

--- src/sections/e-commerce/payment/FormPayment.tsx
+++ src/sections/e-commerce/payment/FormPayment.tsx
@@ -1,118 +1,218 @@
+import { Icon } from '@iconify/react';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function FormPayment() {
-  const navigate = useNavigate();
-
   return (
     <>
-      <div className="flex justify-center pt-20 pb-20">
-        <p
-          onClick={() => navigate('/cart')}
-          className="text-xl text-gray-500 flex justify-between underline-offset-4 cursor-pointer"
-        >
-          Back
-        </p>
-        <form className="border-[1px] shadow-xl w-[50%] p-5 ">
-          <div className="grid md:grid-cols-2 md:gap-6">
-            <div className="mb-1">
-              <label
-                htmlFor="firstname"
-                className="block mb-2 text-sm font-medium text-gray-900 "
-              >
-                Họ
-              </label>
-              <input
-                type="text"
-                id="firstname"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5 focus:border-gray-500 outline-none"
-                required
-              />
-            </div>
-
-            <div className="mb-1">
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-900 "
-              >
-                Mật khẩu
-              </label>
-              <input
-                type="password"
-                id="password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5 focus:border-gray-500 outline-none"
-                required
-              />
-            </div>
+      <div className="pt-5">
+        <div className="bg-white h-[46px] flex items-center">
+          <div className="bg-[#F39801] h-full flex w-[40px]">
+            <img
+              src="https://cdn0.fahasa.com/skin/frontend/ma_vanese/fahasa/images/ico_warning_white.svg?q=10298"
+              className="m-auto"
+              alt="img"
+            />
           </div>
 
-          <div className="grid md:grid-cols-2 md:gap-6">
-            <div className="mb-1 ">
-              <label
-                htmlFor="phone"
-                className="block mb-2 text-sm font-medium text-gray-900 "
-              >
-                Số điện thoại
-              </label>
+          <span className="ms-3 me-1">Bạn đã là thành viên? </span>
+          <Link
+            to={'/login'}
+            className="text-[#F39801] font-bold underline"
+          >
+            Đăng nhập ngay
+          </Link>
+        </div>
+        <div className="bg-white p-5 mt-4">
+          <h3 className="uppercase font-bold text-[14px] border-b-2 pb-2">Địa chỉ giao hàng</h3>
+          <div className="mt-3">
+            <div>
+              <label className="text-[15px] mr-3 w-[170px] inline-block">Họ và tên người nhận</label>
               <input
                 type="text"
-                id="phone"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-gray-500 block w-full p-2.5  outline-none "
+                placeholder="Nhập họ và tên người nhận"
+                className="py-1 text-[14px] font-bold outline-1 outline-blue-300 border-[#ced4da] rounded-sm h-[30px] w-[446px] text-[#495057]"
               />
             </div>
-            <div className="mb-1">
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 "
-              >
-                Email
-              </label>
+            <div className="mt-4">
+              <label className="text-[15px] mr-3 w-[170px] inline-block">Email</label>
               <input
                 type="email"
-                id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5 focus:border-gray-500 outline-none"
-                required
+                placeholder="Nhập email"
+                className="py-1 text-[14px] font-bold outline-1 outline-blue-300 border-[#ced4da] rounded-sm h-[30px] w-[446px] text-[#495057]"
               />
             </div>
-          </div>
-          <div className="grid md:grid-cols gap-2 pt-2">
-            <label
-              htmlFor="noidung"
-              className="block text-sm font-medium text-gray-900 "
-            >
-              Nội dung
-            </label>
-            <textarea
-              id="noidung"
-              rows={4}
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:border-gray-500 outline-none"
-              placeholder="Leave a comment..."
-            ></textarea>
-          </div>
-          <div className="flex items-start pt-2 mb-6">
-            <div className="flex items-center h-5">
+            <div className="mt-4">
+              <label className="text-[15px] mr-3 w-[170px] inline-block">Số điện thoại</label>
               <input
-                id="remember"
-                type="checkbox"
-                value=""
-                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                required
+                type="text"
+                placeholder="Ví dụ: 0979123xxx (10 ký tự số)"
+                maxLength={10}
+                className="py-1 text-[14px] font-bold outline-1 outline-blue-300 border-[#ced4da] rounded-sm h-[30px] w-[446px] text-[#495057]"
               />
             </div>
-            <label
-              htmlFor="remember"
-              className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Remember me
+            <div className="mt-4">
+              <label className="text-[15px] mr-3 w-[170px] inline-block">Tỉnh/Thành Phố</label>
+              <select className="py-1 text-[14px] font-bold outline-1 outline-blue-300 border-[#ced4da] rounded-sm h-[30px] w-[446px] text-[#495057]">
+                <option value="">-- Chọn tỉnh thành --</option>
+              </select>
+            </div>
+            <div className="mt-4">
+              <label className="text-[15px] mr-3 w-[170px] inline-block">Quận/Huyện</label>
+              <select className="py-1 text-[14px] font-bold outline-1 outline-blue-300 border-[#ced4da] rounded-sm h-[30px] w-[446px] text-[#495057]">
+                <option value="">-- Chọn quận huyện --</option>
+              </select>
+            </div>
+            <div className="mt-4">
+              <label className="text-[15px] mr-3 w-[170px] inline-block">Phường/Xã</label>
+              <select className="py-1 text-[14px] font-bold outline-1 outline-blue-300 border-[#ced4da] rounded-sm h-[30px] w-[446px] text-[#495057]">
+                <option value="">-- Chọn phường xã --</option>
+              </select>
+            </div>
+            <div className="mt-4">
+              <label className="text-[15px] mr-3 w-[170px] inline-block">Địa chỉ nhận hàng</label>
+              <input
+                type="text"
+                placeholder="Nhập địa chỉ nhận hàng"
+                className="py-1 text-[14px] font-bold outline-1 outline-blue-300 border-[#ced4da] rounded-sm h-[30px] w-[446px] text-[#495057]"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="bg-white p-5 mt-4">
+          <h3 className="uppercase font-bold text-[14px] border-b-2 pb-2">Phương thức vận chuyển</h3>
+          <div className="mt-3">
+            <label className="font-semibold">
+              <input
+                type="radio"
+                className=""
+              />{' '}
+              Giao hàng tiêu chuẩn: 31.000đ
             </label>
           </div>
-          <button
-            type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Submit
-          </button>
-        </form>
+        </div>
+        <div className="bg-white p-5 mt-4">
+          <h3 className="uppercase font-bold text-[14px] border-b-2 pb-2">Phương thức thanh toán</h3>
+          <div className="mt-3">
+            <label className="text-[14px] block">
+              <input
+                type="radio"
+                name="payment"
+                className=""
+              />{' '}
+              <img
+                src="https://cdn0.fahasa.com/skin/frontend/base/default/images/payment_icon/ico_zalopayapp.svg?q=10298"
+                alt="img"
+                className="inline-block mx-1"
+              />
+              Ví Zalo pay
+            </label>
+            <label className="text-[14px] block mt-4">
+              <input
+                type="radio"
+                name="payment"
+                className=""
+              />{' '}
+              <img
+                src="https://cdn0.fahasa.com/skin/frontend/base/default/images/payment_icon/ico_momopay.svg?q=10298"
+                alt="img"
+                className="inline-block mx-1"
+              />
+              Ví Momo
+            </label>
+            <label className="text-[14px] block mt-4">
+              <input
+                type="radio"
+                name="payment"
+                className=""
+                checked
+              />{' '}
+              <img
+                src="https://cdn0.fahasa.com/skin/frontend/base/default/images/payment_icon/ico_cashondelivery.svg?q=10298"
+                alt="img"
+                className="inline-block mx-1"
+              />
+              Thanh toán bằng tiền mặt khi nhận hàng
+            </label>
+          </div>
+        </div>
+        <div className="bg-white p-5 mt-4">
+          <h3 className="uppercase font-bold text-[14px] border-b-2 pb-2">Mã khuyến mãi/Mã quà tặng</h3>
+          <div className="mt-3">
+            <div className="flex items-center">
+              <label className="text-[15px] inline-block">Mã KM/Quà tặng</label>
+              <span className="mx-3 bg-[#FFB3234D] text-[#FFB323] py-1 px-2 rounded-lg font-bold flex items-center">
+                Chọn mã thành công - Mã giảm giá 10k toàn sàn - đơn hàng 150k
+                <Icon
+                  icon="system-uicons:cross"
+                  className="hover:cursor-pointer ml-1"
+                />
+              </span>
+              <span className="underline text-blue-600">Chọn mã khuyến mãi</span>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white p-5 mt-4">
+          <h3 className="uppercase font-bold text-[14px] border-b-2 pb-2">Kiểm tra lại đơn hàng</h3>
+          <div className="mt-3">
+            <div className="divide-y">
+              <div className="grid grid-cols-5 gap-2 py-2">
+                <div className="max-w-[150px] max-h-[150px]">
+                  <img
+                    src="https://cdn0.fahasa.com/media/catalog/product//8/9/8936066697088.jpg"
+                    className="h-full w-full"
+                    alt="img"
+                  />
+                </div>
+                <div>Lý thuyết trò chơi</div>
+                <div className="text-center">
+                  <p>161.100đ</p>
+                  <p className="text-[13px] line-through text-[#bfbfbf]">179.000đ</p>
+                </div>
+                <div className="text-center">4</div>
+                <div className="text-center">
+                  <p className="text-[#F39801] font-semibold">87.500đ</p>
+                </div>
+              </div>
+              <div className="grid grid-cols-5 gap-2 py-2">
+                <div className="max-w-[150px] max-h-[150px]">
+                  <img
+                    src="https://cdn0.fahasa.com/media/catalog/product//8/9/8936066697088.jpg"
+                    className="h-full w-full"
+                    alt="img"
+                  />
+                </div>
+                <div>Lý thuyết trò chơi</div>
+                <div className="text-center">
+                  <p>161.100đ</p>
+                  <p className="text-[13px] line-through text-[#bfbfbf]">179.000đ</p>
+                </div>
+                <div className="text-center">4</div>
+                <div className="text-center">
+                  <p className="text-[#F39801] font-semibold">87.500đ</p>
+                </div>
+              </div>
+              <div className="grid grid-cols-5 gap-2 py-2">
+                <div className="max-w-[150px] max-h-[150px]">
+                  <img
+                    src="https://cdn0.fahasa.com/media/catalog/product//8/9/8936066697088.jpg"
+                    className="h-full w-full"
+                    alt="img"
+                  />
+                </div>
+                <div>Lý thuyết trò chơi</div>
+                <div className="text-center">
+                  <p>161.100đ</p>
+                  <p className="text-[13px] line-through text-[#bfbfbf]">179.000đ</p>
+                </div>
+                <div className="text-center">4</div>
+                <div className="text-center">
+                  <p className="text-[#F39801] font-semibold">87.500đ</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
