f33e75c62c4276f8f2108edef9f8754e17347e7f
QuocThinh8623 <99307924+QuocThinh8623@users.noreply.github.com>
2023-11-19 15:56:04 +0700

    xong admin user

--- src/sections/e-commerce/Admin/User.tsx
+++ src/sections/e-commerce/Admin/User.tsx
@@ -1,7 +1,74 @@
 import { Icon } from '@iconify/react';
-import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Role } from './enums';
+
+
+interface User {
+  id: number;
+  firstname: string;
+  lastname: string;
+  password: string;
+  email: string;
+  phone: string;
+  address: string;
+  role: string;
+}
+
 
 export default function User() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await axios.get(
+      "http://localhost:8080/api/v1/admin/users",
+      {
+        validateStatus: () => {
+          return true;
+        },
+      }
+    );
+    if (result.status === 302) {
+      setUsers(result.data)
+    }
+  };
+
+  const handlePasswordChange = (index: number, newPassword: string) => {
+    // Giới hạn số ký tự của password là 40
+    if (newPassword.length <= 40) {
+      setUsers(prevUsers => {
+        const updatedUsers = [...prevUsers];
+        updatedUsers[index].password = newPassword;
+        return updatedUsers;
+      });
+    }
+  };
+
+  const updateUserRole = async (userId: number, newRole: Role) => {
+    try {
+      await axios.put(`http://localhost:8080/api/v1/admin/users/update/${userId}`, {
+        role: newRole,
+      });
+      console.log('Vai trò đã được cập nhật thành công.');
+    } catch (error) {
+      console.error('Lỗi khi cập nhật vai trò:', error);
+    }
+  };
+
+  const handleCheckboxChange = (index: number, newValue: boolean) => {
+    setUsers((prevUsers) => {
+      const updatedUsers = [...prevUsers];
+      const newRole = newValue ? 'ADMIN' : 'USER';
+      updatedUsers[index].role = newRole as Role;
+      updateUserRole(updatedUsers[index].id, newRole as Role);
+      return updatedUsers;
+    });
+  };
+
   return (
     <>
       <p className="text-xl pb-5 flex items-center gap-3">
@@ -14,7 +81,7 @@ export default function User() {
         </span>{' '}
       </p>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <table className="w-full text-sm text-gray-500 dark:text-gray-400 text-center">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th
@@ -63,62 +130,51 @@ export default function User() {
                 scope="col"
                 className="px-6 py-3"
               >
+                Vai trò
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3"
+              >
                 Action
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                123abc
-              </th>
-              <td className="px-6 py-4 max-w-[170px]">Nguyen Van</td>
-              <td className="px-6 py-4 max-w-[170px]">An</td>
-              <td className="px-6 py-4 max-w-[170px]">123456</td>
-              <td className="px-6 py-4 max-w-[170px]">klkklkl@gmail.com</td>
-              <td className="px-6 py-4 max-w-[170px]">01234567890</td>
-              <td className="px-6 py-4 max-w-[170px]">Sg Ho Chi Minh</td>
-              <td className="px-6 py-4 max-w-[170px] text-left">
-                <button className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</button>
-              </td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                123abc
-              </th>
-              <td className="px-6 max-w-[170px] py-4">Nguyen Van</td>
-              <td className="px-6 max-w-[170px] py-4">An</td>
-              <td className="px-6 max-w-[170px] py-4">123456</td>
-              <td className="px-6 max-w-[170px] py-4">klkklkl@gmail.com</td>
-              <td className="px-6 max-w-[170px] py-4">01234567890</td>
-              <td className="px-6 max-w-[170px] py-4">Sg Ho Chi Minh</td>
-              <td className="px-6 max-w-[170px] py-4 text-left">
-                <button className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</button>
-              </td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 ">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                123abc
-              </th>
-              <td className="px-6 py-4 max-w-[170px]">Nguyen Van</td>
-              <td className="px-6 py-4 max-w-[170px]">An</td>
-              <td className="px-6 py-4 max-w-[170px]">123456</td>
-              <td className="px-6 py-4 max-w-[170px]">klkklkl@gmail.com</td>
-              <td className="px-6 py-4 max-w-[170px]">01234567890</td>
-              <td className="px-6 py-4 max-w-[170px]">Sg Ho Chi Minh Sg Ho Chi Minh Sg Ho Chi Minh Sg Ho Chi Minh</td>
-              <td className="px-6 py-4 max-w-[170px] text-left">
-                <button className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</button>
-              </td>
-            </tr>
+            {users.map((user, index) => (
+
+              <tr key={user.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700
+               hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th
+                  scope="row" key={index}
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {index + 1}
+                </th>
+                <td className="px-6 py-4 max-w-[170px]">{user.lastname}</td>
+                <td className="px-6 py-4 max-w-[170px]">{user.firstname}</td>
+                <td className="px-6 py-4 max-w-[170px]">
+                  <input disabled className='rounded-lg w-36'
+                    type='password'
+                    value={user.password}
+                    onChange={(e) => handlePasswordChange(index, e.target.value)}
+                    maxLength={40}
+                  />
+                </td>
+                <td className="px-6 py-4 max-w-[170px]">{user.email}</td>
+                <td className="px-6 py-4 max-w-[170px]">{user.phone}</td>
+                <td className="px-6 py-4 max-w-[170px]" >{user.address}</td>
+                <td className="px-6 py-4 max-w-[170px]" >{user.role}</td>
+                <td className="px-6 py-4 max-w-[170px] text-center">
+                  <input
+                    type="checkbox"
+                    checked={user.role === 'ADMIN'}
+                    onChange={(e) => handleCheckboxChange(index, e.target.checked)}
+                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  />
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
